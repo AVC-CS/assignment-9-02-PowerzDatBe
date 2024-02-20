517487803e60f7831ef9e8af4dd09a742a4bedb3
Antonio Narino <anarino@students.avc.edu>
2024-02-20 14:37:56 -0800

    Error in line 30, probably

--- main.hpp
+++ main.hpp
@@ -18,13 +18,38 @@ void printStudent(Student *head);
 
 Student *makeStudent()
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+    Student *head, *temp, *prev;
+    ifstream ifs;
+    ifs.open("students.txt");
+    if(!ifs) {
+        cerr << "ERROR: FILE COOKED" << endl;
+        exit(0);
+    }
+
+    for(int i = 0; i < N; i++) {
+        temp = (Student *)malloc(sizeof(Student));
+        ifs >> temp->id >> temp->name >> temp->major;
+        temp->next = NULL;
+        if (i == 0) {
+            head = temp;
+        }
+        else {
+            prev->next = temp;
+        }
+        prev = temp;
+    }
+
+    return head;
 }
 void printStudent(Student *head)
 {
-    /*******************************
-     * Code your program here
-     *******************************/
+    Student *temp;
+    temp = head;
+    
+    while(temp != NULL) {
+        cout << temp->id << "\t";
+        cout << temp->name << "\t";
+        cout << temp->major << "\n";
+        temp = temp->next;
+    }
 }
